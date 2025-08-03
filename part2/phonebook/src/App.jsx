86611544e14090c0c3ef17fecb4fd55a4ba9ec0b
Devin Weikert <weikert.devin@gmail.com>
2025-08-02 23:17:08 -0400

    Complete exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const handleAddName = (e) => {
+    e.preventDefault();
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+    console.log(`Added ${newPerson.name} to phonebook`);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{margin: '4em'}}>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddName} style={{display: 'flex', flexDirection: 'column'}}>
+        <div >
+          <label>Name: </label>
+          <input 
+            type='text'
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            placeholder='Enter name' 
+          />
+        </div>
+        <div style={{marginTop: '1em'}}>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default App
+export default App;
