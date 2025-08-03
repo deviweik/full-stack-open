7589aa521979ae96f1428b9e130a3dba99ffca83
Devin Weikert <weikert.devin@gmail.com>
2025-08-02 23:40:53 -0400

    Complete exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '+1 404-893-1023' },
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const handleAddName = (e) => {
+  const handleAddPerson = (e) => {
     e.preventDefault();
     if (persons.filter((person) => person.name === newName).length > 0) {
       alert(`${newName} is already added to phonebook`);
@@ -14,17 +15,19 @@ const App = () => {
     }
       
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     setPersons(persons.concat(newPerson));
     setNewName('');
-    console.log(`Added ${newPerson.name} to phonebook`);
+    setNewNumber('');
+    console.log(`Added ${newPerson.name} to phonebook with number ${newPerson.number}`);
   }
 
   return (
     <div style={{margin: '4em'}}>
       <h2>Phonebook</h2>
-      <form onSubmit={handleAddName} style={{display: 'flex', flexDirection: 'column'}}>
+      <form onSubmit={handleAddPerson} style={{display: 'flex', flexDirection: 'column'}}>
         <div >
           <label>Name: </label>
           <input 
@@ -34,16 +37,30 @@ const App = () => {
             placeholder='Enter name' 
           />
         </div>
+        <div>
+          <label>Number: </label>
+          <input 
+            type='text'
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+            placeholder='Enter number' 
+          />
+        </div>
         <div style={{marginTop: '1em'}}>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-      </ul>
+      <table>
+        <tbody>
+          {persons.map((person) => (
+            <tr key={person.name}>
+              <td style={{paddingRight: '1em'}}>{person.name}: </td>
+              <td>{person.number}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
