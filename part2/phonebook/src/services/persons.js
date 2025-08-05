07facd55b4d668e91aaf91c0953cc38ab751d273
Devin Weikert <weikert.devin@gmail.com>
2025-08-05 14:43:12 -0400

    Complete exercise 2.13

---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/persons'
+
+const getPersons = () => {
+    return axios
+        .get(baseUrl)
+        .then(res => res.data)
+}
+
+const addPerson = (newPerson) => {
+    return axios
+        .post(baseUrl, newPerson)
+        .then(res => res.data)
+}
+
+const updatePerson = (id, updatedPerson) => {
+    return axios
+        .put(`${baseUrl}/${id}`, updatedPerson)
+        .then(res => res.data)
+}
+
+const deletePerson = (id) => {
+    return axios
+        .delete(`${baseUrl}/${id}`)
+        .then(res => res.data)
+}
+
+export default { getPersons, addPerson, updatePerson, deletePerson }
