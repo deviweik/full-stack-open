5ebe34767e3ff3dd037f22e7cf9814898784caed
Devin Weikert <weikert.devin@gmail.com>
2025-08-06 09:26:35 -0400

    Complete exercise 2.19

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,6 +2,94 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
+const SearchInput = ({ search, setSearch }) => (
+  <div style={{ marginBottom: '1em', display: 'flex', alignItems: 'center' }}>
+    <h2>Search:</h2>
+    <input
+      style={{ marginLeft: '1em', padding: '0.5em', fontSize: '1em' }}
+      type="text" 
+      value={search} 
+      placeholder='Search for a country' 
+      onChange={(e) => setSearch(e.target.value)} />
+  </div>
+)
+
+const SearchResults = ({ searchedCountries }) => {
+  if (searchedCountries.length === 0) {
+    return (
+      <>
+        <h2>Search Results:</h2>
+        <p>No countries found.</p>
+      </>
+    )
+  }
+  if (searchedCountries.length > 10) {
+    return (
+      <>
+        <h2>Search Results:</h2>
+        <p>Too many matches, specify another filter.</p>
+      </>
+    )
+  }
+  if (searchedCountries.length === 1) {
+    const country = searchedCountries[0];
+    return (
+      <div>
+        <h2>Search Results:</h2>
+        <CountryDetails country={country} />
+      </div>
+    )
+  }
+  return (
+    <>
+      <h2>Search Results:</h2>
+      <ul>
+        {searchedCountries.map(country => (
+          <CountryResultItem 
+            key={country.cca3} 
+            country={country} 
+            onShow={(country) => console.log('Show details for:', country.name.common)} 
+          />
+        ))}
+      </ul>
+    </>
+  )
+}
+
+const CountryResultItem = ({ country }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const onShow = showDetails ? () => setShowDetails(false) : () => setShowDetails(true)
+
+  return (
+    <li>
+      {country.name.common}
+      <button
+        style={{ marginLeft: '1em', padding: '0.3em', fontSize: '1em' }}
+        onClick={onShow}
+      >
+        {showDetails ? 'Hide' : 'Show'}
+      </button>
+      {showDetails && <CountryDetails country={country} />}
+    </li>
+  )
+}
+
+const CountryDetails = ({ country }) => {
+  return (
+    <div>
+      <h3 style={{textDecoration: 'underline'}}>{country.name.common}</h3>
+      <div style={{paddingLeft: '1em'}}>
+        <p><b>Capital:</b> {country.hasOwnProperty('capital') ? country.capital[0] : 'No Capital'}</p>
+        <p><b>Population:</b> {country.population} people</p>
+        <p><b>Area:</b> {country.area} sq km</p>
+        <p><b>Languages:</b> {Object.values(country.languages).join(', ')}</p>
+        <p><b>Flag:</b></p>
+        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+      </div>
+    </div>
+  )
+}
+
 function App() {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
@@ -11,54 +99,18 @@ function App() {
       .get('https://studies.cs.helsinki.fi/restcountries/api/all')
       .then(res => {
         setCountries(res.data)
+        console.log('Countries fetched:', res.data.length)
+        console.log('Countries:', res.data)
       })
   }, [])
 
   const searchedCountries = countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()))
 
-  console.log('countries', countries)
-
   return (
     <>
       <h1>Countries</h1>
-      <div>
-        <h2>Search:</h2>
-        <input 
-          type="text" 
-          value={search} 
-          placeholder='Search for a country' 
-          onChange={(e) => setSearch(e.target.value)} />
-      </div>
-      <div>
-        <h2>Results:</h2>
-        {searchedCountries.length === 0 && <p>No countries found.</p>}
-        {searchedCountries.length > 10 && <p>Too many matches, specify another filter.</p>}
-        {searchedCountries.length <= 10 && searchedCountries.length > 1 && (
-          <ul>
-            {searchedCountries.map(country => (
-              <li key={country.cca3}>
-                {country.name.common}
-              </li>
-            ))}
-          </ul>
-        )}
-        {searchedCountries.length === 1 && (
-          <div>
-            <h3>{searchedCountries[0].name.common}</h3>
-            <p>Capital: {searchedCountries[0].capital[0]}</p>
-            <p>Population: {searchedCountries[0].population} people</p>
-            <p>Area: {searchedCountries[0].area} sq km</p>
-            <h4>Languages:</h4>
-            <ul>
-              {Object.values(searchedCountries[0].languages).map((language) => (
-                <li key={language}>{language}</li>
-              ))}
-            </ul>
-            <h4>Flag:</h4>
-            <img src={searchedCountries[0].flags.png} alt={`Flag of ${searchedCountries[0].name.common}`} />
-          </div>
-        )}
-      </div>
+      <SearchInput search={search} setSearch={setSearch} />
+      <SearchResults searchedCountries={searchedCountries} />
       {/* {<button onClick={() => console.log(searchedCountries[0])}>Log Country</button>} */}
     </>
   )
