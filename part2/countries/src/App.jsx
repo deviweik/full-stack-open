aaff884dd8826cf7ffccdb24e95762a796fbf709
Devin Weikert <weikert.devin@gmail.com>
2025-08-06 11:57:12 -0400

    Complete exercise 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import weatherCodes from './assets/weather_codes.json'
 import './App.css'
 
 const SearchInput = ({ search, setSearch }) => (
@@ -75,6 +76,28 @@ const CountryResultItem = ({ country }) => {
 }
 
 const CountryDetails = ({ country }) => {
+  const latlng = country.capitalInfo?.latlng
+  const url = latlng
+    ? `https://api.open-meteo.com/v1/forecast?latitude=${latlng[0]}&longitude=${latlng[1]}&current=temperature_2m,precipitation,wind_speed_10m,is_day,weather_code&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`
+    : null
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    if (!url) {
+      setWeather(null)
+      return
+    }
+    axios
+      .get(url)
+      .then(res => {
+        setWeather(res.data?.current)
+        console.log('Weather data fetched:', res.data)
+      })
+      .catch(err => {
+        console.error('Error fetching weather data:', err)
+      })
+  }, [url]) // Fetch weather data when the URL changes
+
   return (
     <div>
       <h3 style={{textDecoration: 'underline'}}>{country.name.common}</h3>
@@ -85,6 +108,36 @@ const CountryDetails = ({ country }) => {
         <p><b>Languages:</b> {Object.values(country.languages).join(', ')}</p>
         <p><b>Flag:</b></p>
         <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+        <WeatherDetails capital={country?.capital[0]} weather={weather} />
+      </div>
+    </div>
+  )
+}
+
+const WeatherDetails = ({ capital, weather }) => {
+  if (!weather) {
+    return <p>Loading weather data...</p>
+  }
+
+  const codeInfo = weatherCodes[weather.weather_code] || {}
+  const weatherCodeDesc = weather.is_day ? codeInfo.day : codeInfo.night || "Unknown"
+
+  return (
+    <div>
+      <h3>Weather in {capital}</h3>
+      <div style={{paddingLeft: '1em'}}>
+        <p><b>Temperature:</b> {weather.temperature_2m}°F</p>
+        <p><b>Day/Night:</b> {weather.is_day ? "Daytime" : "Nighttime"}</p>
+        <p><b>Precipitation:</b> {weather.precipitation} inches</p>
+        <p><b>Wind Speed:</b> {weather.wind_speed_10m} mph</p>
+        <div style={{ display: 'flex', alignItems: 'center'}}>
+          <p><b>Condition:</b> {weatherCodeDesc.description}</p>
+          <img
+            src={weatherCodeDesc.image}
+            alt={weatherCodeDesc.description + ' icon'}
+            style={{ width: '32px', height: '32px', marginLeft: '0.5em', objectFit: 'contain' }}
+          />
+        </div>
       </div>
     </div>
   )
