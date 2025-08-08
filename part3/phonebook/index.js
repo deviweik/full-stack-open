42f5f87c258c6d5d14ff18ea1bd8506e19a03569
Devin Weikert <weikert.devin@gmail.com>
2025-08-08 18:08:16 -0400

    Complete exercise 3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,8 @@ const morgan = require('morgan')
 
 const app = express();
 
-app.use(morgan('tiny'))
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(express.json());
 
